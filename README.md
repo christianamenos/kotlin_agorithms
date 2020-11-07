47707d776194ec23bb51a8e35dba15824cb1cd28
Christian <christian.amenos@gmail.com>
2020-11-07 15:13:02 +0100

    Added code for day 1 of 30 day challenge in hacker rank

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kotlin algorithms
+
+This repository contains the solution to little exercises from HackerRank or other platforms in Kotlin. The idea is to
+learn Kotlin by practice though the resolution of small coding exercises.
+
---
+++ src/main/kotlin/hackerrankChallenge/day00.kt
@@ -0,0 +1,11 @@
+package hackerrankChallenge
+
+// Link: https://www.hackerrank.com/challenges/30-hello-world/problem
+
+fun main() {
+    /* Read input from STDIN. Print output to STDOUT. */
+    println("Please, enter your name:")
+    val userName = readLine()!!
+    print("Welcome to day 0 $userName!!")
+}
+
